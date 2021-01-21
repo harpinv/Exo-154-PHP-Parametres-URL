a11bc7e0eb8914a00c6db400699b20a20782b08f
Nux007 <007.nux@gmail.com>
2021-01-21 17:02:30 +0100

    Exo 154.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Exo 1
+ * ------
+ * 1. Créez une nouvelle page 'exo1.php'
+ * 2. Créez un lien dans index.php ( ici ) qui passe le paramètre 'iteration' avec pour valeur 10 à cette page.
+ * 3. Traitez tous les paramètres pour vous prémunir des petits malins.
+ * 4. Faites une boucle pour afficher autant de fois Hello World 10 fois en utilisant 'iteration' dans la boucle.
+ */
+
+// TODO Votre code ici.
+
+
+/**
+ * Exo 2
+ * ------
+ * Faites exactement la même chose que pour l'exercice 1, mais le nouveau fichier sera nommé exo2.php
+ * Créez au moins 10 liens à l'aide d'une boucle !!!
+ * Pour chaque lien, le paramètre iteration prendra la valeur actuelle de votre variable de boucle ( $i par exemple ).
+ */
+
+
+
+
+
